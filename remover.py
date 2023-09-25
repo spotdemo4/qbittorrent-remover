042c7460b495283b2967f7df363191f49d49fab3
spotdemo4 <racierjet@hotmail.com>
2023-09-24 23:01:47 -0400

    fix: try catch around connection

--- remover.py
+++ remover.py
@@ -6,13 +6,16 @@ load_dotenv()
 
 while True:
     print('Checking for torrents to remove')
-    qb = Client(os.getenv('QB_URL'))
-    qb.login(os.getenv('QB_USER'), os.getenv('QB_PASS'))
-    torrents = qb.torrents()
+    try:
+        qb = Client(os.getenv('QB_URL'))
+        qb.login(os.getenv('QB_USER'), os.getenv('QB_PASS'))
+        torrents = qb.torrents()
 
-    for torrent in torrents:
-        if torrent['category'] == 'autobrr' and torrent['state'] == 'pausedUP':
-            print('Deleting ' + torrent['name'])
-            qb.delete_permanently(torrent['hash'])
+        for torrent in torrents:
+            if torrent['category'] == 'autobrr' and torrent['state'] == 'pausedUP':
+                print('Deleting ' + torrent['name'])
+                qb.delete_permanently(torrent['hash'])
+    except:
+        print('Error connecting to qBittorrent')
 
     time.sleep(3600)
